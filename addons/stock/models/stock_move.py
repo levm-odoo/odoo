c83fc71bf93c4bc17ba6eefeec9fb7ac63956054
Arnold Moyaux <arm@odoo.com>
2024-11-19 17:56:50 +0000

    Cherry pick of f72251e83fd21a234cbf475d3f1b41df8deb85f1 failed
    stdout:
    Auto-merging addons/stock/models/stock_move.py
    CONFLICT (content): Merge conflict in addons/stock/models/stock_move.py
    
    stderr:

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1864,13 +1864,24 @@ Please change the quantity done or the rounding precision of your unit of measur
         # self cannot contain moves that are either cancelled or done, therefore we can safely
         # unlink all associated move_line_ids
         moves_to_cancel._do_unreserve()
+        cancel_moves_origin = self.env['ir.config_parameter'].sudo().get_param('stock.cancel_moves_origin')
+
+        moves_to_cancel.state = 'cancel'
 
         for move in moves_to_cancel:
             siblings_states = (move.move_dest_ids.mapped('move_orig_ids') - move).mapped('state')
             if move.propagate_cancel:
                 # only cancel the next move if all my siblings are also cancelled
                 if all(state == 'cancel' for state in siblings_states):
+<<<<<<< saas-17.2
                     move.move_dest_ids.filtered(lambda m: m.state != 'done' and m.location_dest_id == m.move_dest_ids.location_id)._action_cancel()
+||||||| 09e0a8aec404501c0898072d701340e5c0ce1deb
+                    move.move_dest_ids.filtered(lambda m: m.state != 'done')._action_cancel()
+=======
+                    move.move_dest_ids.filtered(lambda m: m.state != 'done')._action_cancel()
+                    if cancel_moves_origin:
+                        move.move_orig_ids.sudo().filtered(lambda m: m.state != 'done')._action_cancel()
+>>>>>>> ab392c5172433d7f9b8de56abf7a13b23319051f
             else:
                 if all(state in ('done', 'cancel') for state in siblings_states):
                     move_dest_ids = move.move_dest_ids
@@ -1879,7 +1890,6 @@ Please change the quantity done or the rounding precision of your unit of measur
                         'move_orig_ids': [Command.unlink(move.id)]
                     })
         moves_to_cancel.write({
-            'state': 'cancel',
             'move_orig_ids': [(5, 0, 0)],
             'procure_method': 'make_to_stock',
         })
