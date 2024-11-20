66868233bd6534be8d6a7af7844d63a5cdbf369c
Logan Staelens <lost@odoo.com>
2024-11-20 16:05:07 +0100

    [IMP] base: Extending Qweb to support rendering xml from files only
    This commit add 1 new feature to Qweb that gives the possibility to render templates from files using a path as template name.
    This is used to prevent the user from changing templates. We want that specifically with calls to external api under our name as we want to keep our request as intended.
    It also allows to change file template in stable and so avoid the stable policy.
    
    The file template must be in a specific subfolder named templates
    
    A template name should look like this when targetting a file `l10n_uk_cis/templates/transaction_cis_body.xml`

--- odoo/addons/base/tests/test_qweb.py
+++ odoo/addons/base/tests/test_qweb.py
@@ -14,7 +14,7 @@ from textwrap import dedent
 
 from odoo.tests.common import TransactionCase
 from odoo.addons.base.models.ir_qweb import QWebException, render
-from odoo.tools import misc, mute_logger
+from odoo.tools import file_open, misc, mute_logger
 from odoo.tools.json import scriptsafe as json_scriptsafe
 from odoo.exceptions import UserError, ValidationError, MissingError
 
@@ -1704,6 +1704,39 @@ class TestQWebBasic(TransactionCase):
             """
         })
         self.env['ir.qweb'].with_context(lang='pt_BR')._render(view1.id, {})  # should not crash
+
+    def test_render_template_from_file(self):
+        expected_result = etree.fromstring(file_open('base/tests/file_template/file_expected_render.xml').read())
+        rendered_result = self.env['ir.qweb']._render('base/tests/file_template/templates/file_template.xml', values={
+            'document_name': 'Test Document',
+            'partner': {
+                'name': 'Jerry',
+                'forename': 'Khan',
+            },
+        })
+        self.assertEqual(etree.fromstring(rendered_result), expected_result)
+
+    def test_render_template_from_file_special_cases(self):
+        with self.assertRaises(FileNotFoundError):
+            self.env['ir.qweb']._render('base/tests/file_template/templates/../file_template.xml', values={})
+        self.env['ir.qweb']._render('base/tests/file_template/templates/../templates/file_template.xml', values={
+            'document_name': 'Test Document',
+            'partner': {
+                'name': 'Jerry',
+                'forename': 'Khan',
+            },
+        })
+
+        self.env['ir.qweb']._render('./base/tests//file_template/templates/file_template.xml', values={
+            'document_name': 'Test Document',
+            'partner': {
+                'name': 'Jerry',
+                'forename': 'Khan',
+            },
+        })
+
+        with self.assertRaises(FileNotFoundError):
+            self.env['ir.qweb']._render('base/tests/file_template/unreadable_file_template.xml', values={})
 
     def test_void_element(self):
         view = self.env['ir.ui.view'].create({
