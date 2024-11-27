28928b8ce7727ab6e26598e2475b2923919674ef
Louis (loti) <loti@odoo.com>
2024-11-27 17:21:44 +0100

    [FIX] website_sale*: fix max combo quantity
    Before this fix, it was possible to add more combo products than available
    to the cart. Indeed, the available quantity was only checked in the combo
    configurator, but not in other places where the quantity could be updated
    (such as the product page or the cart).
    
    This fix introduces a "max quantity" concept for combo products, which is based
    on the available quantity of their combo items.
    
    opw-4378554

---
+++ addons/website_sale_stock/models/product_combo.py
@@ -0,0 +1,15 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class ProductCombo(models.Model):
+    _inherit = 'product.combo'
+
+    def _get_max_available_quantity(self, website, **kwargs):
+        self.ensure_one()
+        max_quantities = [
+            item.product_id._get_max_available_quantity(website, **kwargs)
+            for item in self.combo_item_ids
+        ]
+        return max(max_quantities) if (None not in max_quantities) else None
--- addons/website_sale_stock/models/product_product.py
+++ addons/website_sale_stock/models/product_product.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, _
+from odoo import _, fields, models
 from odoo.http import request
 
 
@@ -15,14 +14,19 @@ class ProductProduct(models.Model):
         return partner in self.stock_notification_partner_ids
 
     def _get_cart_qty(self, website=None):
-        if not self.allow_out_of_stock_order:
-            website = website or self.env['website'].get_current_website()
-            # When the cron is run manually, request has no attribute website, and that would cause a crash
-            # so we check for it
-            cart = website and request and hasattr(request, 'website') and website.sale_get_order() or None
-            if cart:
-                return sum(cart._get_common_product_lines(product=self).mapped('product_uom_qty'))
+        website = website or self.env['website'].get_current_website()
+        cart = website and request and website.sale_get_order() or None
+        if cart:
+            return sum(cart._get_common_product_lines(product=self).mapped('product_uom_qty'))
         return 0
+
+    def _get_max_available_quantity(self, website, **kwargs):
+        self.ensure_one()
+        if self.is_storable and not self.allow_out_of_stock_order:
+            available_qty = website._get_product_available_qty(self.sudo(), **kwargs)
+            cart_quantity = self._get_cart_qty(website)
+            return available_qty - cart_quantity
+        return None
 
     def _is_sold_out(self):
         self.ensure_one()
