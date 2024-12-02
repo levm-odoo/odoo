6d368fc2603b49476af664ecd96311167995c856
qsm-odoo <qsm@odoo.com>
2024-12-02 14:52:01 +0100

    2

--- addons/website/static/src/snippets/s_scroll_progress/000.js
+++ addons/website/static/src/snippets/s_scroll_progress/000.js
@@ -4,22 +4,36 @@ import { throttleForAnimation } from "@web/core/utils/timing";
 
 const ScrollProgress = publicWidget.Widget.extend({
     selector: '.s_scroll_progress',
+    disabledInEditableMode: false,
 
     start() {
-        document.querySelector('header').appendChild(this.el);
+        this.options.wysiwyg?.odooEditor.observerUnactive();
+        if (!this.el.dataset.position) {
+            document.querySelector('header').appendChild(this.el);
+        } else {
+            this.el.classList.add('fixed-bottom');
+        }
+        this.options.wysiwyg?.odooEditor.observerActive();
 
         this.__onScroll = throttleForAnimation(this._onScroll.bind(this));
         document.addEventListener('scroll', this.__onScroll);
+
+        // Make sure to handle the initial scroll (may not be 0 on editor start
+        // or when applying a new position).
+        this._onScroll();
 
         return this._super(...arguments);
     },
 
     destroy() {
+        this.options.wysiwyg?.odooEditor.observerUnactive();
         document.querySelector('main #wrap').prepend(this.el);
+        this.el.classList.remove('fixed-bottom');
 
         document.removeEventListener('scroll', this.__onScroll);
 
         this.el.querySelector('.s_scroll_progress_bar').style.width = '';
+        this.options.wysiwyg?.odooEditor.observerActive();
 
         return this._super(...arguments);
     },
