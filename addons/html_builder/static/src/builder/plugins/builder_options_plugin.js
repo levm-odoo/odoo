980c3a41bcb4af817042e1d3a241ce5cba159f01
FrancoisGe <fge@odoo.com>
2024-12-09 13:35:02 +0100

    WIP overlay visibility

--- addons/html_builder/static/src/builder/plugins/builder_options_plugin.js
+++ addons/html_builder/static/src/builder/plugins/builder_options_plugin.js
@@ -1,4 +1,5 @@
 import { Plugin } from "@html_editor/plugin";
+import { reactive } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { uniqueId } from "@web/core/utils/functions";
 
@@ -7,6 +8,7 @@ export class BuilderOptionsPlugin extends Plugin {
     static dependencies = ["selection", "overlay"];
     resources = {
         selectionchange_handlers: this.onSelectionChange.bind(this),
+        step_added_handlers: this.updateOptionContainers.bind(this),
     };
 
     setup() {
@@ -19,6 +21,8 @@ export class BuilderOptionsPlugin extends Plugin {
             }
             this.changeSidebarTarget(e.target);
         });
+
+        this.currentOptionsContainers = reactive([]);
     }
 
     onSelectionChange(selection) {
@@ -35,11 +39,11 @@ export class BuilderOptionsPlugin extends Plugin {
         this.changeSidebarTarget(selectionNode);
     }
 
-    changeSidebarTarget(selectedElement) {
+    getMapOptions() {
         const map = new Map();
         for (const option of this.builderOptions) {
             const { selector } = option;
-            const elements = getClosestElements(selectedElement, selector);
+            const elements = getClosestElements(this.currentSelectedElement, selector);
             for (const element of elements) {
                 if (map.has(element)) {
                     map.get(element).push(option);
@@ -48,19 +52,67 @@ export class BuilderOptionsPlugin extends Plugin {
                 }
             }
         }
-        const optionsContainers = [...map]
-            .sort(([a], [b]) => {
-                return b.contains(a) ? 1 : -1;
-            })
-            .map(([element, options]) => ({ element, options, id: uniqueId() }));
+        return map;
+    }
+
+    changeSidebarTarget(selectedElement) {
+        this.currentSelectedElement = selectedElement;
+        this.updateOptionContainers();
         for (const handler of this.getResource("change_current_options_containers_listeners")) {
-            handler(optionsContainers);
+            handler(this.currentOptionsContainers);
         }
         return;
+    }
+
+    updateOptionContainers() {
+        const map = this.getMapOptions();
+        const elementsWithContainer = new Set(
+            this.currentOptionsContainers.map((optionsContainer) => optionsContainer.element)
+        );
+
+        for (const optionContainer of this.currentOptionsContainers) {
+            for (const option of optionContainer.options) {
+                option.isVisible = optionContainer.element.matches(option.selector);
+            }
+        }
+
+        const elementsToAdd = [...map.keys()].filter((el) => !elementsWithContainer.has(el));
+        for (const element of elementsToAdd) {
+            const options = map.get(element);
+            this.currentOptionsContainers.push({
+                id: uniqueId(),
+                options: this.getOptions(options),
+                element,
+            });
+        }
+
+        const elementsToRemove = [...elementsWithContainer].filter((el) => !map.has(el));
+        for (const element of elementsToRemove) {
+            const index = this.currentOptionsContainers.findIndex(
+                (container) => element === container.element
+            );
+            this.currentOptionsContainers.splice(index, 1);
+        }
+
+        if (elementsToAdd.length) {
+            this.currentOptionsContainers.sort((a, b) => (b.element.contains(a.element) ? 1 : -1));
+        }
+    }
+
+    getOptions(options) {
+        const optionsSet = new Set(options);
+        return this.builderOptions.map((option) => ({
+            ...option,
+            isVisible: optionsSet.has(option),
+        }));
     }
 }
 
 function getClosestElements(element, selector) {
+    if (!element) {
+        // TODO we should remove it
+        return [];
+    }
     const parent = element.closest(selector);
     return parent ? [parent, ...getClosestElements(parent.parentElement, selector)] : [];
 }
