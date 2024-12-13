5aa7dfdb2164546ae5ae6e858e1aad6097d41713
visp-odoo <visp@odoo.com>
2024-12-13 15:36:14 +0530

    [IMP] website: convert website utils
    Removing Jquery from website_utils as this file is independent of
    public widget.
    
    task-3770362

--- addons/website_sale/static/src/js/website_sale.js
+++ addons/website_sale/static/src/js/website_sale.js
@@ -249,7 +249,9 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, cartHandlerM
                     const zoomOdoo = image.dataset.zoomOdoo;
                     if (zoomOdoo) {
                         zoomOdoo.hide();
-                        image.removeEventListener();
+                        // Common way to remove all event listeners is to replace the element with a clone of itself
+                        const newElement = image.cloneNode(true); // Deep clone (includes child nodes)
+                        image.parentNode.replaceChild(newElement, image);
                     }
                 });
                 if (image.complete) {
