b5bffdcb078b5f12f6ce29f9e2da75ae696b795a
sami odoo <sami@odoo.com>
2024-12-17 12:27:19 +0530

    [IMP] web:  notify  users about invalid field
    Work in Progress
    
    Task-3981155

--- addons/web/static/src/model/relational_model/record.js
+++ addons/web/static/src/model/relational_model/record.js
@@ -1,5 +1,4 @@
 import { markRaw, markup, toRaw } from "@odoo/owl";
-import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
 import { _t } from "@web/core/l10n/translation";
 import { x2ManyCommands } from "@web/core/orm_service";
@@ -992,7 +991,8 @@ export class Record extends DataPoint {
                 this.data[fieldName]._abandonRecords();
             }
         }
-        if (!this._checkValidity({ displayNotification: true })) {
+        const notify = this.isNew || !this.selected;
+        if (!this._checkValidity({ displayNotification: notify })) {
             return false;
         }
         const changes = this._getChanges();
@@ -1141,20 +1141,6 @@ export class Record extends DataPoint {
         const canProceed = this.model.hooks.onWillSetInvalidField(this, fieldName);
         if (canProceed === false) {
             return;
-        }
-        if (
-            this.selected &&
-            this.model.multiEdit &&
-            this.model.root._recordToDiscard !== this &&
-            !this._invalidFields.has(fieldName)
-        ) {
-            await this.model.dialog.add(AlertDialog, {
-                body: _t("No valid record to save"),
-                confirm: async () => {
-                    await this.discard();
-                    this.switchMode("readonly");
-                },
-            });
         }
         this._invalidFields.add(fieldName);
     }
