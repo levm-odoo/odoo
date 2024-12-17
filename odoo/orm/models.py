a81a45d4ab9fc952f2dd644932bad2ae90b19356
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-12-17 14:03:13 +0000

    [REF] move TransientModel into its own file

--- odoo/orm/models.py
+++ odoo/orm/models.py
@@ -57,7 +57,7 @@ from odoo.tools import (
     ormcache, partition, Query, split_every, unique,
     SQL, sql,
 )
-from odoo.tools.constants import GC_UNLINK_LIMIT, PREFETCH_MAX
+from odoo.tools.constants import PREFETCH_MAX
 from odoo.tools.lru import LRU
 from odoo.tools.misc import LastOrderedSet, ReversedIterable, unquote
 from odoo.tools.translate import _, LazyTranslate
@@ -467,8 +467,7 @@ class BaseModel(metaclass=MetaModel):
     If set to ``False``, override :meth:`~odoo.models.BaseModel.init`
     to create the database table.
 
-    Automatically defaults to `True` for :class:`Model` and
-    :class:`TransientModel`, `False` for :class:`AbstractModel`.
+    Automatically defaults to `True` for abstract models.
 
     .. tip:: To create a model without any table, inherit
             from :class:`~odoo.models.AbstractModel`.
@@ -7255,76 +7254,6 @@ class Model(AbstractModel):
     _register = False           # not visible in ORM registry, meant to be python-inherited only
     _abstract = False           # not abstract
     _transient = False          # not transient
-
-
-class TransientModel(Model):
-    """ Model super-class for transient records, meant to be temporarily
-    persistent, and regularly vacuum-cleaned.
-
-    A TransientModel has a simplified access rights management, all users can
-    create new records, and may only access the records they created. The
-    superuser has unrestricted access to all TransientModel records.
-    """
-    _auto = True                # automatically create database backend
-    _register = False           # not visible in ORM registry, meant to be python-inherited only
-    _abstract = False           # not abstract
-    _transient = True           # transient
-
-    @api.autovacuum
-    def _transient_vacuum(self):
-        """Clean the transient records.
-
-        This unlinks old records from the transient model tables whenever the
-        :attr:`_transient_max_count` or :attr:`_transient_max_hours` conditions
-        (if any) are reached.
-
-        Actual cleaning will happen only once every 5 minutes. This means this
-        method can be called frequently (e.g. whenever a new record is created).
-
-        Example with both max_hours and max_count active:
-
-        Suppose max_hours = 0.2 (aka 12 minutes), max_count = 20, there are
-        55 rows in the table, 10 created/changed in the last 5 minutes, an
-        additional 12 created/changed between 5 and 10 minutes ago, the rest
-        created/changed more than 12 minutes ago.
-
-        - age based vacuum will leave the 22 rows created/changed in the last 12
-          minutes
-        - count based vacuum will wipe out another 12 rows. Not just 2,
-          otherwise each addition would immediately cause the maximum to be
-          reached again.
-        - the 10 rows that have been created/changed the last 5 minutes will NOT
-          be deleted
-        """
-        if self._transient_max_hours:
-            # Age-based expiration
-            self._transient_clean_rows_older_than(self._transient_max_hours * 60 * 60)
-
-        if self._transient_max_count:
-            # Count-based expiration
-            self._transient_clean_old_rows(self._transient_max_count)
-
-    def _transient_clean_old_rows(self, max_count):
-        # Check how many rows we have in the table
-        self._cr.execute(SQL("SELECT count(*) FROM %s", SQL.identifier(self._table)))
-        [count] = self._cr.fetchone()
-        if count > max_count:
-            self._transient_clean_rows_older_than(300)
-
-    def _transient_clean_rows_older_than(self, seconds):
-        # Never delete rows used in last 5 minutes
-        seconds = max(seconds, 300)
-        self._cr.execute(SQL(
-            "SELECT id FROM %s WHERE %s < %s %s",
-            SQL.identifier(self._table),
-            SQL("COALESCE(write_date, create_date, (now() AT TIME ZONE 'UTC'))::timestamp"),
-            SQL("(now() AT TIME ZONE 'UTC') - interval %s", f"{seconds} seconds"),
-            SQL(f"LIMIT { GC_UNLINK_LIMIT }"),
-        ))
-        ids = [x[0] for x in self._cr.fetchall()]
-        self.sudo().browse(ids).unlink()
-        if len(ids) >= GC_UNLINK_LIMIT:
-            self.env.ref('base.autovacuum_job')._trigger()
 
 
 def itemgetter_tuple(items):
