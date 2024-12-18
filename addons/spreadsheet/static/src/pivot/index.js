0652da5d4169a73bc5ab1f6542a4ac566fd7ccf8
Pierre Rousseau <pro@odoo.com>
2024-12-18 09:03:54 +0100

    wip

--- addons/spreadsheet/static/src/pivot/index.js
+++ addons/spreadsheet/static/src/pivot/index.js
@@ -2,7 +2,7 @@ import { _t } from "@web/core/l10n/translation";
 
 import * as spreadsheet from "@odoo/o-spreadsheet";
 
-import { SEE_RECORDS_PIVOT, SEE_RECORDS_PIVOT_VISIBLE } from "./pivot_actions";
+import { ADD_PIVOT_FILTER_CHILDREN, SEE_RECORDS_PIVOT, SEE_RECORDS_PIVOT_VISIBLE } from "./pivot_actions";
 import { PivotOdooCorePlugin } from "./plugins/pivot_odoo_core_plugin";
 import { PivotUIGlobalFilterPlugin } from "./plugins/pivot_ui_global_filter_plugin";
 
@@ -32,6 +32,17 @@ cellMenuRegistry.add("pivot_see_records", {
         return SEE_RECORDS_PIVOT_VISIBLE(position, env.model.getters);
     },
     icon: "o-spreadsheet-Icon.SEE_RECORDS",
+});
+
+cellMenuRegistry.add("pivot_add_filter", {
+    name: _t("Add filter"),
+    sequence: 180,
+    children: [ADD_PIVOT_FILTER_CHILDREN],
+    isVisible: (env) => {
+        const position = env.model.getters.getActivePosition();
+        return SEE_RECORDS_PIVOT_VISIBLE(position, env.model.getters);
+    },
+    icon: "o-spreadsheet-Icon.GLOBAL_FILTERS",
 });
 
 inverseCommandRegistry.add("UPDATE_ODOO_PIVOT_DOMAIN", identity);
--- addons/spreadsheet/static/src/pivot/pivot_actions.js
+++ addons/spreadsheet/static/src/pivot/pivot_actions.js
@@ -1,8 +1,43 @@
 // @ts-check
 
+import { globalFiltersFieldMatchers } from "@spreadsheet/global_filters/plugins/global_filters_core_plugin";
 import { navigateTo } from "../actions/helpers";
 import { helpers } from "@odoo/o-spreadsheet";
 const { getNumberOfPivotFunctions } = helpers;
+const uuidGenerator = new helpers.UuidGenerator();
+
+function getUniqueLabel(label, condition) {
+    let newLabel = label;
+    let counter = 1;
+    while (condition(newLabel)) {
+        newLabel = `${label} (${counter})`;
+        counter++;
+    }
+    return newLabel;
+}
+
+function getLabel(model, field, env, type) {
+    let label = `${model} / ${field}`;
+    if (getType(type) === "relation") {
+        label = field;
+    }
+    return getUniqueLabel(label, (label) => env.model.getters.getGlobalFilterLabel(label));
+}
+
+function getType(type) {
+    switch (type) {
+        case "integer": //ID
+        case "many2one":
+        case "many2many":
+        case "one2many":
+            return "relation";
+        case "datetime":
+        case "date":
+            return "date";
+        default:
+            return "text";
+    }
+}
 
 /**
  * @param {import("@odoo/o-spreadsheet").CellPosition} position
@@ -88,4 +123,65 @@ export function SET_FILTER_MATCHING(position, env) {
     const domain = env.model.getters.getPivotCellFromPosition(position).domain;
     const filters = env.model.getters.getFiltersMatchingPivotArgs(pivotId, domain);
     env.model.dispatch("SET_MANY_GLOBAL_FILTER_VALUE", { filters });
+}
+
+export function ADD_PIVOT_FILTER(position, env) {
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    const domain = env.model.getters.getPivotCellFromPosition(position).domain;
+    debugger;
+}
+
+export function ADD_PIVOT_FILTER_CHILDREN(env) {
+    const position = env.model.getters.getActivePosition();
+    const pivotId = env.model.getters.getPivotIdFromPosition(position);
+    const domain = env.model.getters.getPivotCellFromPosition(position).domain;
+    const pivot = env.model.getters.getPivot(pivotId);
+    return domain.map((node, index) => ({
+        id: `add_pivot_filter_${node.field}`,
+        name: pivot.getFields()[node.field].string,
+        sequence: index,
+        execute: async (env) => {
+            const field = node.field;
+            const model = pivot.coreDefinition.model;
+            const type = getType(pivot.getFields()[field].type);
+            const modelName = type === "relation" ? pivot.getFields()[field].relation : undefined;
+            const id = uuidGenerator.uuidv4();
+            const additionalPayload = {};
+            for (const [type, el] of Object.entries(globalFiltersFieldMatchers)) {
+                additionalPayload[type] = {};
+                for (const objectId of el.getIds()) {
+                    if (el.getModel(objectId) !== model) {
+                        continue;
+                    }
+                    additionalPayload[type][objectId] = {
+                        chain: field,
+                        type: pivot.getFields()[field].type,
+                    };
+                }
+            }
+            env.model.dispatch("ADD_GLOBAL_FILTER", {
+                filter: {
+                    id,
+                    label: getLabel(model, pivot.getFields()[field].string, env, pivot.getFields()[field].type),
+                    type,
+                    modelName,
+                    irModel: model,
+                    irField: field,
+                },
+                ...additionalPayload,
+            });
+            switch (type) {
+                case "text":
+                    env.openSidePanel("TEXT_FILTER_SIDE_PANEL", { id });
+                    break;
+                case "date":
+                    env.openSidePanel("DATE_FILTER_SIDE_PANEL", { id });
+                    break;
+                case "relation":
+                    env.openSidePanel("RELATION_FILTER_SIDE_PANEL", { id });
+                    break;
+            }
+        },
+        isVisible: (env) => env.model.getters.getPivot(pivotId).isValid(),
+    }));
 }
