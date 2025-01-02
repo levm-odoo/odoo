3d14dc5d7a9e355e1d14ffd29511f40f7d2915b5
Andrzej(pian) <pian@odoo.com>
2025-01-02 18:41:44 +0100

    [FIX] digest: convert kpi to users company currency
    Assuming that incoming kps are given in the digest.company.currency field
    and that we want to give them in the user.company.currency (recipient's company's currency)
    we need to convert them to the user's company currency before sending the digest email.
    
    Reproduce
    ---
    
    - have two companies
    	- usd_company (currency set to USD)
    		- usd_company has usd_user
    	- aed_company (currency set to AED)
    		- aed_company uses "United Arab Emirates" Fiscal Package
    		- aed_company has a aed_usd_digest (in aed company but sent to "usd users"):
    			- all_digest KPIs are "all sales" and "revenue"
    			- usd_user is a recipient of the aed_usd_digest
    - admin makes a sale order for 100 AED (105 with tax), confirms it and creates an invoice
    - Digest email is sent to usd_user, recent revenue is shown in USD which is desired,
        but it isn't converted correctly (105 AED is around 28 USD )
    
    opw-4399055

--- addons/account/tests/test_digest.py
+++ addons/account/tests/test_digest.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from lxml import html
+
 from odoo.addons.digest.tests.common import TestDigestCommon
 from odoo.tools import mute_logger
 from odoo.tests import tagged
@@ -63,4 +65,45 @@ class TestAccountDigest(TestDigestCommon):
             int(self.digest_3.with_company(self.company_2).kpi_account_total_revenue_value),
             -2,
             msg='When no company is set, the KPI must be computed based on the current company',
+        )
+
+    def test_kpi_currency_follows_recipients_company_currency(self):
+        # Have a recipient thats company has a different currency than the digest's company's currency
+        self.user_employee_c2.name = "Employee in Company using AED currency"
+        company_aed = self.env['res.company'].create({'name': 'Digest Company AED', 'currency_id': self.env.ref('base.AED').id})
+        self.user_employee_c2.write({'company_ids': [(4, company_aed.id)]})
+
+        self.user_employee_c2.company_id = company_aed
+        self.digest_1.user_ids = self.user_employee_c2
+        self.user_employee_c2.groups_id |= self.env.ref('account.group_account_invoice')
+
+        # for the sake of simplicity: reduce digest kpis to revenue only
+        self.digest_1.kpi_account_total_revenue = True
+        self.digest_1.kpi_mail_message_total = False
+        self.digest_1.kpi_res_users_connected = False
+
+        self.env['res.currency.rate'].create({
+            'name': '2025-01-03',
+            'currency_id': self.digest_1.currency_id.id,
+            'rate': 0.1,
+            'company_id': company_aed.id,
+        })
+
+        # digest creates its mails in auto_delete mode so we need to capture
+        # the formatted body during the sending process
+        self.digest_1.flush_recordset()
+        with self.mock_mail_gateway():
+            self.digest_1.action_send()
+
+        self.assertEqual(len(self._new_mails), 1, "A new mail.mail should have been created")
+        mail = self._new_mails[0]
+        # check mail.mail content
+        self.assertEqual(mail.email_to, self.user_employee_c2.email_formatted)
+
+        kpi_message_values = html.fromstring(mail.body_html).xpath('//span[contains(@class, "kpi_value") and contains(@class, "kpi_border_col")]/text()')
+
+        self.assertEqual(
+            [t.strip() for t in kpi_message_values],
+            ['0د.إ', '-130د.إ', '-130د.إ'],
+            "The digest should display the KPI values in the recipient's company currency"
         )
