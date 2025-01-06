7908a0a3ceff0003c7c34f1c078ca3d23e50e770
Yousef Alnashef (alny) <alny@odoo.com>
2025-01-06 06:44:29 +0000

    [IMP] l10n_sa, l10n_sa_edi: Improved Report Spacing
    ticket ID: 4342267
    
    -Fix printing draft invoice with invoice_date as today's date
    
    -Fix report spacing header by adding a new A4 paperformat for l10n_sa which is set as the saudi arabia companies default paper format.
    
    -Fix exchange rate table to show both arabic and english labels and remove unnecessary translations (no longer used)
    
    -Remove unnecessary spans in xpath.
    
    X-original-commit: b01bcd79f9599c278755092340a9860b3ca6bb6c

--- addons/l10n_sa/__manifest__.py
+++ addons/l10n_sa/__manifest__.py
@@ -17,6 +17,19 @@ Odoo Arabic localization for most Saudi Arabia.
     'data': [
         'data/account_data.xml',
         'data/account_tax_report_data.xml',
+<<<<<<< 17.0
+||||||| 062cc7fc71324253f7f6fc5c8aa10561cf13400f
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
+        'data/account_chart_template_configure_data.xml',
+        'views/view_move_form.xml',
+=======
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
+        'data/account_chart_template_configure_data.xml',
+        'data/report_paperformat_data.xml',
+        'views/view_move_form.xml',
+>>>>>>> b18537e7d43e5675d4dc20b431ce9ed9cbd01414
         'views/report_invoice.xml',
     ],
     'demo': [
