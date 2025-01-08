c70550cdd9f9df2b0fba2a57b0ffccf30ba7d38c
Walid (wasa) <wasa@odoo.com>
2025-01-08 17:03:02 +0100

    [FIX] web_editor: extract link from HTML
    **Problem**:
    When a link is copied with HTML content, `handlePasteText` is called after `handlePasteHtml`, and the content type is `text/html`.
    
    **Solution**:
    Extract the pasted content as text inside `handlePasteHtml` and check if it is a single valid link. If so, skip further processing.
    
    **Steps to Reproduce**:
    1. Copy a link from Visual Studio Code.
    2. Paste the link into the editor.
    3. Observe that the link is not created.
    
    opw-4460599

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -260,6 +260,10 @@ export class ClipboardPlugin extends Plugin {
      */
     handlePasteOdooEditorHtml(clipboardData) {
         const odooEditorHtml = clipboardData.getData("application/vnd.odoo.odoo-editor");
+        const textContent = clipboardData.getData("text/plain");
+        if (/^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/i.test(textContent)) {
+            return false;
+        }
         if (odooEditorHtml) {
             const fragment = parseHTML(this.document, odooEditorHtml);
             this.dependencies.sanitize.sanitize(fragment);
@@ -276,6 +280,10 @@ export class ClipboardPlugin extends Plugin {
     handlePasteHtml(selection, clipboardData) {
         const files = getImageFiles(clipboardData);
         const clipboardHtml = clipboardData.getData("text/html");
+        const textContent = clipboardData.getData("text/plain");
+        if (/^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/i.test(textContent)) {
+            return false;
+        }
         if (files.length || clipboardHtml) {
             const clipboardElem = this.prepareClipboardData(clipboardHtml);
             // @phoenix @todo: should it be handled in table plugin?
