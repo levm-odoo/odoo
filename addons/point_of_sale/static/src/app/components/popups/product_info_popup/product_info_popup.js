88b9884840f5af668fbe15af320f618bf22f2994
Jitendra Prajapat <jipr@odoo.com>
2025-01-09 12:24:27 +0530

    [IMP] point_of_sale:  manage favorite products directly from the frontend
    In this commit:
    ===============
    Users can now mark or unmark products as favorites directly from the POS
    frontend. Reduces the need for user redirection from the frontend to the backend
    for managing favorite products.
    
    Task-4452738

--- addons/point_of_sale/static/src/app/components/popups/product_info_popup/product_info_popup.js
+++ addons/point_of_sale/static/src/app/components/popups/product_info_popup/product_info_popup.js
@@ -1,6 +1,6 @@
 import { Dialog } from "@web/core/dialog/dialog";
 import { usePos } from "@point_of_sale/app/hooks/pos_hook";
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { ProductInfoBanner } from "@point_of_sale/app/components/product_info_banner/product_info_banner";
 
 export class ProductInfoPopup extends Component {
@@ -10,6 +10,9 @@ export class ProductInfoPopup extends Component {
 
     setup() {
         this.pos = usePos();
+        this.state = useState({
+            isFavorite: this.props.productTemplate.is_favorite,
+        });
     }
     searchProduct(productName) {
         this.pos.setSelectedCategory(0);
@@ -28,5 +31,16 @@ export class ProductInfoPopup extends Component {
     }
     get allowProductEdition() {
         return true; // Overrided in pos_hr
+    }
+    toggleFavorite() {
+        this.state.isFavorite = !this.state.isFavorite;
+    }
+    confirm() {
+        if (this.state.isFavorite !== this.props.productTemplate.is_favorite) {
+            this.pos.data.write("product.template", [this.props.productTemplate.id], {
+                is_favorite: this.state.isFavorite,
+            });
+        }
+        this.props.close();
     }
 }
