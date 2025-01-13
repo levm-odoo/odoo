keyboard-slayer/odoo-eval

[REF] website(_*)_sale(_*): cart access
In all ecommerce flows, the cart was found, and created
through the `sale_get_order` util, which not only found
the cart, but also updated it according to the current
request configuration.

This led recently to a big issue where all the ecommerce
orders became assigned to the superuser (aka OdooBot) because
the method was called with a modified environment, with the
superuser.

This big issue highlighted once again this method, which
doesn't work as you'd expect it to and was one of the
cornerstones of the ecommerce flows that needed to be
adapted to avoid such issues, clarify its uses, ...

The first idea was to follow the existing approach done
for the pricelist and the fiscal_position, through computed
fields on the website, as a form of 'request cache', but
as the field result cannot be sudoed by default like the
result of `sale_get_order` was, this means all code would
have to be aware of it to avoid forgetting to sudo the field
result.

> request.website.sale_order_id.sudo()

After more thoughts, inspiration came from the existing `website`
attribute on the request, since the order (and pricelist) are
cached in the request session, and are not expected to change
during most requests (except specific ones).

> request.cart (sudo)
> request.pricelist
> request.fiscal_position (sudo)

All three records can be empty, if a cart is needed, it must be
created clearly through the `_create_cart` method, replacing
the previous `force_create` argument of `sale_get_order`.

This new approach:
* provides a clear single access to the existing cart (if any)
* ensures that the cart checks and automatic updates
(update the address after login, removing archived products, ...)
are only done once by request, with the request environment.
Local updates to the environment in routes won't trigger unexpected changes
to the cart.
Routes and method overrides won't trigger the check again when fetching
the cart, like it was the case previously w

addons/website_sale/tests/test_website_sale_cart_payment.py
@@ -5,19 +5,16 @@ from odoo.tests.common import JsonRpcException, tagged
 from odoo.tools import mute_logger
 
 from odoo.addons.payment.tests.http_common import PaymentHttpCommon
-from odoo.addons.website.tools import MockRequest
+from odoo.addons.website_sale.tests.common import WebsiteSaleCommon, mock_website_sale_request
 
 
 @tagged('post_install', '-at_install')
-class WebsiteSaleCartPayment(PaymentHttpCommon):
+class WebsiteSaleCartPayment(PaymentHttpCommon, WebsiteSaleCommon):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.website = cls.env['website'].get_current_website()
-        with MockRequest(cls.env, website=cls.website):
-            cls.order = cls.website.sale_get_order(force_create=True)  # Create the cart to retrieve
         cls.tx = cls.env['payment.transaction'].create({
             'payment_method_id': cls.payment_method_id,
             'amount': cls.amount,
@@ -27,17 +24,18 @@ class WebsiteSaleCartPayment(PaymentHttpCommon):
             'operation': 'online_redirect',
             'partner_id': cls.partner.id,
         })
-        cls.order.write({'transaction_ids': [Command.set([cls.tx.id])]})
+        cls.cart.write({'transaction_ids': [Command.set([cls.tx.id])]})
 
     def test_unpaid_orders_can_be_retrieved(self):
         """ Test that fetching sales orders linked to a payment transaction in the states 'draft',
         'cancel', or 'error' returns the orders. """
         for unpaid_order_tx_state in ('draft', 'cancel', 'error'):
             self.tx.state = unpaid_order_tx_state
-            with MockRequest(self.env, website=self.website, sale_order_id=self.order.id):
+            website = self.website.with_user(self.public_user)
+            with mock_website_sale_request(website.env, website=website, sale_order_id=self.cart.id) as req:
                 self.assertEqual(
-                    self.website.sale_get_order(),
-                    self.order,
+                    req.cart,
+                    self.cart,
                     msg=f"The transaction state '{unpaid_order_tx_state}' should not prevent "
                         f"retrieving the linked order.",
                 )
@@ -48,18 +46,18 @@ class WebsiteSaleCartPayment(PaymentHttpCommon):
         self.tx.provider_id.support_manual_capture = 'full_only'
         for paid_order_tx_state in ('pending', 'authorized', 'done'):
             self.tx.state = paid_order_tx_state
-            with MockRequest(self.env, website=self.website, sale_order_id=self.order.id):
+            with mock_website_sale_request(self.env, website=self.website, sale_order_id=self.cart.id) as req:
                 self.assertFalse(
-                    self.website.sale_get_order(),
+                    req.website._get_and_cache_current_order(),
                     msg=f"The transaction state '{paid_order_tx_state}' should prevent retrieving "
                         f"the linked order.",
                 )
 
     @mute_logger('odoo.http')
     def test_transaction_route_rejects_unexpected_kwarg(self):
-        url = self._build_url(f'/shop/payment/transaction/{self.order.id}')
+        url = self._build_url(f'/shop/payment/transaction/{self.cart.id}')
         route_kwargs = {
-            'access_token': self.order._portal_ensure_token(),
+            'access_token': self.cart._portal_ensure_token(),
             'partner_id': self.partner.id,  # This should be rejected.
         }
         with self.assertRaises(JsonRpcException, msg='odoo.exceptions.ValidationError'):

addons/website_sale/tests/test_website_sale_pricelist.py
@@ -6,8 +6,7 @@ from odoo.fields import Command
 from odoo.tests import tagged
 
 from odoo.addons.base.tests.common import HttpCaseWithUserPortal, TransactionCaseWithUserDemo
-from odoo.addons.website_sale.tests.common import WebsiteSaleCommon
-
+from odoo.addons.website_sale.tests.common import WebsiteSaleCommon, mock_website_sale_request
 
 r''' /!\/!\
 Calling `get_pricelist_available` after setting `property_product_pricelist` on
@@ -206,7 +205,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'list_price': 100,
             'taxes_id': False,
         })
-        self.website.pricelist_id.write({
+        self.pricelist.write({
             'item_ids': [Command.clear(), Command.create({
                 'applied_on': '1_product',
                 'product_tmpl_id': product.product_tmpl_id.id,
@@ -221,7 +220,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
                 'applied_on': '1_product',
                 'product_tmpl_id': product.product_tmpl_id.id,
                 'base': 'pricelist',
-                'base_pricelist_id': self.website.pricelist_id.id,
+                'base_pricelist_id': self.pricelist.id,
                 'compute_price': 'percentage',
                 'percent_price': 25,
             })]
@@ -229,6 +228,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
         so = self.env['sale.order'].create({
             'partner_id': self.env.user.partner_id.id,
             'website_id': self.website.id,
+            'pricelist_id': self.pricelist.id,
             'order_line': [Command.create({
                 'name': product.name,
                 'product_id': product.id,
@@ -252,7 +252,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'list_price': 0,
             'taxes_id': False,
         })
-        self.website.pricelist_id.write({
+        self.pricelist.write({
             'item_ids': [
                 Command.clear(),
                 Command.create({
@@ -267,7 +267,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
         so = self.env['sale.order'].create({
             'partner_id': self.env.user.partner_id.id,
             'website_id': self.website.id,
-            'pricelist_id': self.website.pricelist_id.id,
+            'pricelist_id': self.pricelist.id,
             'order_line': [Command.create({
                 'name': product.name,
                 'product_id': product.id,
@@ -307,28 +307,29 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'name': 'Product Template', 'list_price': 10.0, 'standard_price': 5.0
         })
         self.assertEqual(product_template.standard_price, 5)
-        # Hack to enforce the use of this pricelist in the call to `_get_sales_price`
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        msg = "Template has no variants, the price should be computed based on the template's cost."
-        self.assertEqual(price, 4.5, msg)
+        with mock_website_sale_request(
+            self.env, website=self.website, website_sale_current_pl=pricelist.id
+        ) as req:
+            self.assertEqual(req.pricelist, pricelist)
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            msg = "Template has no variants, the price should be computed based on the template's cost."
+            self.assertEqual(price, 4.5, msg)
 
-        product_template.attribute_line_ids = [Command.create({
-            'attribute_id': pa.id, 'value_ids': [Command.set([pav1.id, pav2.id])]
-        })]
-        msg = "Product template with variants should have no cost."
-        self.assertEqual(product_template.standard_price, 0, msg)
-        self.assertEqual(product_template.product_variant_ids[0].standard_price, 0)
+            product_template.attribute_line_ids = [Command.create({
+                'attribute_id': pa.id, 'value_ids': [Command.set([pav1.id, pav2.id])]
+            })]
+            msg = "Product template with variants should have no cost."
+            self.assertEqual(product_template.standard_price, 0, msg)
+            self.assertEqual(product_template.product_variant_ids[0].standard_price, 0)
 
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        msg = "Template has variants, the price should be computed based on the 1st variant's cost."
-        self.assertEqual(price, 0, msg)
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            msg = "Template has variants, the price should be computed based on the 1st variant's cost."
+            self.assertEqual(price, 0, msg)
 
-        product_template.product_variant_ids[0].standard_price = 20
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        self.assertEqual(price, 18, msg)
+            product_template.product_variant_ids[0].standard_price = 20
+
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            self.assertEqual(price, 18, msg)
 
     def test_base_price_with_discount_on_pricelist_tax_included(self):
         """
@@ -363,10 +364,12 @@ class TestWebsitePriceList(WebsiteSaleCommon):
                 'product_tmpl_id': product_tmpl.id,
             })],
         })
-        # Hack to enforce the use of this pricelist in the call to `_get_sales_price`
-        self.website.pricelist_id = self.pricelist
-        res = product_tmpl._get_sales_prices(self.website)
-        self.assertEqual(res[product_tmpl.id]['base_price'], 75)
+        with mock_website_sale_request(
+            self.website.env, website=self.website, website_sale_current_pl=self.pricelist.id
+        ) as req:
+            self.assertEqual(req.pricelist, self.pricelist)
+            res = product_tmpl._get_sales_prices(self.website)
+            self.assertEqual(res[product_tmpl.id]['base_price'], 75)
 
 def simulate_frontend_context(self, website_id=1):
     # Mock this method will be enough to simulate frontend context in most methods
@@ -537,8 +540,10 @@ class TestWebsitePriceListAvailableGeoIP(TestWebsitePriceListAvailable):
         pls_to_return += self.env.user.partner_id.property_product_pricelist
 
         current_pl = self.w1_pl_code
-        with patch('odoo.addons.website_sale.models.website.Website._get_geoip_country_code', return_value=self.BE.code), \
-            patch('odoo.addons.website_sale.models.website.Website._get_cached_pricelist_id', return_value=current_pl.id):
+        with (
+            patch('odoo.addons.website_sale.models.website.Website._get_geoip_country_code', return_value=self.BE.code),
+            mock_website_sale_request(self.env, website=self.website, website_sale_current_pl=current_pl.id),
+        ):
             pls = self.website.get_pricelist_available(show_visible=True)
         self.assertEqual(pls, pls_to_return + current_pl, "Only pricelists for BE, accessible en website and selectable should be returned. It should also return the applied promo pl")
 

levm-odoo/odoo

addons/website_sale/tests/test_website_sale_cart_payment.py
@@ -5,19 +5,16 @@ from odoo.tests.common import JsonRpcException, tagged
 from odoo.tools import mute_logger
 
 from odoo.addons.payment.tests.http_common import PaymentHttpCommon
-from odoo.addons.website.tools import MockRequest
+from odoo.addons.website_sale.tests.common import MockRequest, WebsiteSaleCommon
 
 
 @tagged('post_install', '-at_install')
-class WebsiteSaleCartPayment(PaymentHttpCommon):
+class WebsiteSaleCartPayment(PaymentHttpCommon, WebsiteSaleCommon):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.website = cls.env['website'].get_current_website()
-        with MockRequest(cls.env, website=cls.website):
-            cls.order = cls.website.sale_get_order(force_create=True)  # Create the cart to retrieve
         cls.tx = cls.env['payment.transaction'].create({
             'payment_method_id': cls.payment_method_id,
             'amount': cls.amount,
@@ -27,17 +24,17 @@ class WebsiteSaleCartPayment(PaymentHttpCommon):
             'operation': 'online_redirect',
             'partner_id': cls.partner.id,
         })
-        cls.order.write({'transaction_ids': [Command.set([cls.tx.id])]})
+        cls.cart.write({'transaction_ids': [Command.set([cls.tx.id])]})
 
     def test_unpaid_orders_can_be_retrieved(self):
         """ Test that fetching sales orders linked to a payment transaction in the states 'draft',
         'cancel', or 'error' returns the orders. """
         for unpaid_order_tx_state in ('draft', 'cancel', 'error'):
             self.tx.state = unpaid_order_tx_state
-            with MockRequest(self.env, website=self.website, sale_order_id=self.order.id):
+            with MockRequest(self.env, website=self.website, sale_order_id=self.cart.id) as request:
                 self.assertEqual(
-                    self.website.sale_get_order(),
-                    self.order,
+                    request.cart,
+                    self.cart,
                     msg=f"The transaction state '{unpaid_order_tx_state}' should not prevent "
                         f"retrieving the linked order.",
                 )
@@ -48,18 +45,18 @@ class WebsiteSaleCartPayment(PaymentHttpCommon):
         self.tx.provider_id.support_manual_capture = 'full_only'
         for paid_order_tx_state in ('pending', 'authorized', 'done'):
             self.tx.state = paid_order_tx_state
-            with MockRequest(self.env, website=self.website, sale_order_id=self.order.id):
+            with MockRequest(self.env, website=self.website, sale_order_id=self.cart.id) as request:
                 self.assertFalse(
-                    self.website.sale_get_order(),
+                    request.website._get_and_cache_current_order(),
                     msg=f"The transaction state '{paid_order_tx_state}' should prevent retrieving "
                         f"the linked order.",
                 )
 
     @mute_logger('odoo.http')
     def test_transaction_route_rejects_unexpected_kwarg(self):
-        url = self._build_url(f'/shop/payment/transaction/{self.order.id}')
+        url = self._build_url(f'/shop/payment/transaction/{self.cart.id}')
         route_kwargs = {
-            'access_token': self.order._portal_ensure_token(),
+            'access_token': self.cart._portal_ensure_token(),
             'partner_id': self.partner.id,  # This should be rejected.
         }
         with self.assertRaises(JsonRpcException, msg='odoo.exceptions.ValidationError'):

addons/website_sale/tests/test_website_sale_pricelist.py
@@ -10,7 +10,7 @@ from odoo.addons.base.tests.common import (
     HttpCaseWithUserPortal,
     TransactionCaseWithUserDemo,
 )
-from odoo.addons.website_sale.tests.common import WebsiteSaleCommon
+from odoo.addons.website_sale.tests.common import MockRequest, WebsiteSaleCommon
 
 
 r''' /!\/!\
@@ -210,7 +210,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'list_price': 100,
             'taxes_id': False,
         })
-        self.website.pricelist_id.write({
+        self.pricelist.write({
             'item_ids': [Command.clear(), Command.create({
                 'applied_on': '1_product',
                 'product_tmpl_id': product.product_tmpl_id.id,
@@ -225,7 +225,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
                 'applied_on': '1_product',
                 'product_tmpl_id': product.product_tmpl_id.id,
                 'base': 'pricelist',
-                'base_pricelist_id': self.website.pricelist_id.id,
+                'base_pricelist_id': self.pricelist.id,
                 'compute_price': 'percentage',
                 'percent_price': 25,
             })]
@@ -233,6 +233,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
         so = self.env['sale.order'].create({
             'partner_id': self.env.user.partner_id.id,
             'website_id': self.website.id,
+            'pricelist_id': self.pricelist.id,
             'order_line': [Command.create({
                 'name': product.name,
                 'product_id': product.id,
@@ -255,7 +256,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'list_price': 0,
             'taxes_id': False,
         })
-        self.website.pricelist_id.write({
+        self.pricelist.write({
             'item_ids': [
                 Command.clear(),
                 Command.create({
@@ -270,7 +271,7 @@ class TestWebsitePriceList(WebsiteSaleCommon):
         so = self.env['sale.order'].create({
             'partner_id': self.env.user.partner_id.id,
             'website_id': self.website.id,
-            'pricelist_id': self.website.pricelist_id.id,
+            'pricelist_id': self.pricelist.id,
             'order_line': [Command.create({
                 'name': product.name,
                 'product_id': product.id,
@@ -309,28 +310,29 @@ class TestWebsitePriceList(WebsiteSaleCommon):
             'name': 'Product Template', 'list_price': 10.0, 'standard_price': 5.0
         })
         self.assertEqual(product_template.standard_price, 5)
-        # Hack to enforce the use of this pricelist in the call to `_get_sales_price`
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        msg = "Template has no variants, the price should be computed based on the template's cost."
-        self.assertEqual(price, 4.5, msg)
+        with MockRequest(
+            self.env, website=self.website, website_sale_current_pl=pricelist.id
+        ) as request:
+            self.assertEqual(request.pricelist, pricelist)
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            msg = "Template has no variants, the price should be computed based on the template's cost."
+            self.assertEqual(price, 4.5, msg)
 
-        product_template.attribute_line_ids = [Command.create({
-            'attribute_id': pa.id, 'value_ids': [Command.set([pav1.id, pav2.id])]
-        })]
-        msg = "Product template with variants should have no cost."
-        self.assertEqual(product_template.standard_price, 0, msg)
-        self.assertEqual(product_template.product_variant_ids[0].standard_price, 0)
+            product_template.attribute_line_ids = [Command.create({
+                'attribute_id': pa.id, 'value_ids': [Command.set([pav1.id, pav2.id])]
+            })]
+            msg = "Product template with variants should have no cost."
+            self.assertEqual(product_template.standard_price, 0, msg)
+            self.assertEqual(product_template.product_variant_ids[0].standard_price, 0)
 
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        msg = "Template has variants, the price should be computed based on the 1st variant's cost."
-        self.assertEqual(price, 0, msg)
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            msg = "Template has variants, the price should be computed based on the 1st variant's cost."
+            self.assertEqual(price, 0, msg)
 
-        product_template.product_variant_ids[0].standard_price = 20
-        self.website.pricelist_id = pricelist
-        price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
-        self.assertEqual(price, 18, msg)
+            product_template.product_variant_ids[0].standard_price = 20
+
+            price = product_template._get_sales_prices(self.website)[product_template.id]['price_reduce']
+            self.assertEqual(price, 18, msg)
 
     def test_base_price_with_discount_on_pricelist_tax_included(self):
         """
@@ -365,10 +367,12 @@ class TestWebsitePriceList(WebsiteSaleCommon):
                 'product_tmpl_id': product_tmpl.id,
             })],
         })
-        # Hack to enforce the use of this pricelist in the call to `_get_sales_price`
-        self.website.pricelist_id = self.pricelist
-        res = product_tmpl._get_sales_prices(self.website)
-        self.assertEqual(res[product_tmpl.id]['base_price'], 75)
+        with MockRequest(
+            self.website.env, website=self.website, website_sale_current_pl=self.pricelist.id
+        ) as request:
+            self.assertEqual(request.pricelist, self.pricelist)
+            res = product_tmpl._get_sales_prices(self.website)
+            self.assertEqual(res[product_tmpl.id]['base_price'], 75)
 
 def simulate_frontend_context(self, website_id=1):
     # Mock this method will be enough to simulate frontend context in most methods
@@ -566,8 +570,10 @@ class TestWebsitePriceListAvailableGeoIP(TestWebsitePriceListAvailable):
         pls_to_return += self.env.user.partner_id.property_product_pricelist
 
         current_pl = self.w1_pl_code
-        with patch('odoo.addons.website_sale.models.website.Website._get_geoip_country_code', return_value=self.BE.code), \
-            patch('odoo.addons.website_sale.models.website.Website._get_cached_pricelist_id', return_value=current_pl.id):
+        with (
+            patch('odoo.addons.website_sale.models.website.Website._get_geoip_country_code', return_value=self.BE.code),
+            MockRequest(self.env, website=self.website, website_sale_current_pl=current_pl.id),
+        ):
             pls = self.website.get_pricelist_available(show_visible=True)
         self.assertEqual(pls, pls_to_return + current_pl, "Only pricelists for BE, accessible en website and selectable should be returned. It should also return the applied promo pl")
 