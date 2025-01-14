d3696b94bab8d1d83a686a2138586a8364c0a70c
Aaron Bohy <aab@odoo.com>
2025-01-14 10:28:47 +0100

    [wip]

--- addons/web/static/src/webclient/menus/menu_service.js
+++ addons/web/static/src/webclient/menus/menu_service.js
@@ -1,13 +1,48 @@
 import { browser } from "@web/core/browser/browser";
 import { registry } from "@web/core/registry";
 
+import { router } from "@web/core/browser/router";
+import { Component, xml } from "@odoo/owl";
+
+export class ClearMenus extends Component {
+    static template = xml`<button t-on-click="onClick">Clear cache</button>`;
+    static props = {};
+    onClick() {
+        browser.localStorage.removeItem("webclient_menus");
+    }
+}
+registry
+    .category("systray")
+    .add("web.clear_menus", { Component: ClearMenus }, { sequence: 999999 });
+
+router.addLockedKey("delay");
+export class Delay extends Component {
+    static template = xml`<button t-on-click="onClick">Delay</button>`;
+    static props = {};
+    onClick() {
+        if (router.current.delay) {
+            router.pushState({ delay: "0" });
+        } else {
+            router.pushState({ delay: "200" });
+        }
+    }
+}
+registry.category("systray").add("web.delay", { Component: Delay }, { sequence: 999999 });
+
+export class Reload extends Component {
+    static template = xml`<button t-on-click="onClick">Reload</button>`;
+    static props = {};
+    onClick() {
+        browser.location.reload();
+    }
+}
+registry.category("systray").add("web.reload", { Component: Reload }, { sequence: 999999 });
+
 export const menuService = {
     dependencies: ["action", "orm"],
     async start(env, { orm }) {
         const _fetchMenus = async () => {
-            if (window.slow) {
-                await new Promise((r) => setTimeout(r, 3000))
-            }
+            await new Promise((r) => setTimeout(r, router.current.delay));
             return orm.call("ir.ui.menu", "load_web_menus", [!!odoo.debug]);
         };
 
