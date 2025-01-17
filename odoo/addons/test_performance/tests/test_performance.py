7bed6371e9bd2820a349a88b44efe11c86fb6a61
Raphael Collet <rco@odoo.com>
2025-01-17 16:13:27 +0100

    [IMP] test_performance: set query counts back

--- odoo/addons/test_performance/tests/test_performance.py
+++ odoo/addons/test_performance/tests/test_performance.py
@@ -261,20 +261,12 @@ class TestPerformance(SavepointCaseWithUserDemo):
         rec1 = self.env['test_performance.base'].create({'name': 'X'})
 
         # create N lines on rec1: O(1) queries
-        with self.assertQueriesContain([
-            """ INSERT INTO "test_performance_line" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(3):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.create({'value': 0})]})
         self.assertEqual(len(rec1.line_ids), 1)
 
-        with self.assertQueriesContain([
-            """ INSERT INTO "test_performance_line" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(3):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.create({'value': val}) for val in range(1, 12)]})
         self.assertEqual(len(rec1.line_ids), 12)
@@ -282,49 +274,23 @@ class TestPerformance(SavepointCaseWithUserDemo):
         lines = rec1.line_ids
 
         # update N lines: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ UPDATE "test_performance_line" SET "value" """,
-        ]):
+        with self.assertQueryCount(3):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.update(line.id, {'value': 42}) for line in lines[0]]})
         self.assertEqual(rec1.line_ids, lines)
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ UPDATE "test_performance_line" SET "value" """,
-        ]):
+        with self.assertQueryCount(3):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.update(line.id, {'value': 42 + line.id}) for line in lines[1:]]})
         self.assertEqual(rec1.line_ids, lines)
 
         # delete N lines: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ DELETE FROM "test_performance_line" WHERE """,
-            """ SELECT "ir_model_data"."id" FROM "ir_model_data" WHERE """,
-            """ SELECT id FROM ir_attachment WHERE """,
-            """ SELECT "ir_default"."id" FROM "ir_default" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(8):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.delete(line.id) for line in lines[0]]})
         self.assertEqual(rec1.line_ids, lines[1:])
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ DELETE FROM "test_performance_line" WHERE """,
-            """ SELECT "ir_model_data"."id" FROM "ir_model_data" WHERE """,
-            """ SELECT id FROM ir_attachment WHERE """,
-            """ SELECT "ir_default"."id" FROM "ir_default" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(8):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.delete(line.id) for line in lines[1:]]})
         self.assertFalse(rec1.line_ids)
@@ -334,30 +300,12 @@ class TestPerformance(SavepointCaseWithUserDemo):
         lines = rec1.line_ids
 
         # unlink N lines: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ DELETE FROM "test_performance_line" WHERE """,
-            """ SELECT "ir_model_data"."id" FROM "ir_model_data" WHERE """,
-            """ SELECT id FROM ir_attachment WHERE """,
-            """ SELECT "ir_default"."id" FROM "ir_default" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(8):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.unlink(line.id) for line in lines[0]]})
         self.assertEqual(rec1.line_ids, lines[1:])
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ DELETE FROM "test_performance_line" WHERE """,
-            """ SELECT "ir_model_data"."id" FROM "ir_model_data" WHERE """,
-            """ SELECT id FROM ir_attachment WHERE """,
-            """ SELECT "ir_default"."id" FROM "ir_default" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(8):
             self.env.invalidate_all()
             rec1.write({'line_ids': [Command.unlink(line.id) for line in lines[1:]]})
         self.assertFalse(rec1.line_ids)
@@ -368,67 +316,35 @@ class TestPerformance(SavepointCaseWithUserDemo):
         rec2 = self.env['test_performance.base'].create({'name': 'X'})
 
         # link N lines from rec1 to rec2: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_line" SET "base_id" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(5):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.link(line.id) for line in lines[0]]})
         self.assertEqual(rec1.line_ids, lines[1:])
         self.assertEqual(rec2.line_ids, lines[0])
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_line" SET "base_id" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(5):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.link(line.id) for line in lines[1:]]})
         self.assertFalse(rec1.line_ids)
         self.assertEqual(rec2.line_ids, lines)
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(2):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.link(line.id) for line in lines[0]]})
         self.assertEqual(rec2.line_ids, lines)
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(2):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.link(line.id) for line in lines[1:]]})
         self.assertEqual(rec2.line_ids, lines)
 
         # empty N lines in rec2: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id" FROM "test_performance_line" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-            """ DELETE FROM "test_performance_line" WHERE """,
-            """ SELECT "ir_model_data"."id" FROM "ir_model_data" WHERE """,
-            """ SELECT id FROM ir_attachment WHERE """,
-            """ SELECT "ir_default"."id" FROM "ir_default" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(9):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.clear()]})
         self.assertFalse(rec2.line_ids)
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id" FROM "test_performance_line" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(3):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.clear()]})
         self.assertFalse(rec2.line_ids)
@@ -437,36 +353,19 @@ class TestPerformance(SavepointCaseWithUserDemo):
         lines = rec1.line_ids
 
         # set N lines in rec2: O(1) queries
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id" FROM "test_performance_line" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_line" SET "base_id" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(5):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.set(lines[0].ids)]})
         self.assertEqual(rec1.line_ids, lines[1:])
         self.assertEqual(rec2.line_ids, lines[0])
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id" FROM "test_performance_line" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_line" SET "base_id" """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(5):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.set(lines.ids)]})
         self.assertFalse(rec1.line_ids)
         self.assertEqual(rec2.line_ids, lines)
 
-        with self.assertQueriesContain([
-            """ SELECT "test_performance_line"."id" FROM "test_performance_line" WHERE """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ SELECT "test_performance_line"."id", "test_performance_line"."base_id", """,
-            """ UPDATE "test_performance_base" SET "total" """,
-        ]):
+        with self.assertQueryCount(4):
             self.env.invalidate_all()
             rec2.write({'line_ids': [Command.set(lines.ids)]})
         self.assertEqual(rec2.line_ids, lines)
