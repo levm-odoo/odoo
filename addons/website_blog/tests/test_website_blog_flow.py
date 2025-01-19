levm-odoo/odoo

commit 06346b049a3a13a9a8b63a4f60151ec24fabb59e

[FIX] base: fix update translations
for model_terms translations, update_field_translations uses
{lang: {old_lang_term, new_lang_term}} to update translations
This api is not designed for
1. one en_term to be translated to multiple different lang_terms
2. multiple en_terms to be translated to the same lang_term
When the above corner cases happen, it would be impossible to rollback without
the help of the `write` api.

For example
when the translations become
{
    "en_US": "<div> en1 </div><div> en2 <div>"
    "fr_FR": "<div> fr1 </div><div> fr1 <div>"
}
there is no way to make the two terms in fr_FR become different again. Unless
the user `write` the value `with_context(lang="fr_FR")`.

It is acceptable for most fields as a workaround, but not ir.ui.view.arch_db,
which we only display its logical en_US value to the user. There is no UI to
directly modify the value with specified language.

This commit changes the api of `update_field_translations` and uses
{lang: {old_en_term, new_lang_term}} to update translations.
In the above example, the user can just call
`update_field_translations(field_name, {"fr_FR": {"en2": "fr2"}})`

Part-of: odoo/odoo#170779
Signed-off-by: Raphael Collet <rco@odoo.com>

addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1240,7 +1240,7 @@ export class Wysiwyg extends Component {
                 observerData.observer.observe(observerData.field, observerOptions);
             }
         } else {
-            const odooFieldSelector = '[data-oe-model], [data-oe-translation-initial-sha]';
+            const odooFieldSelector = '[data-oe-model], [data-oe-translation-source-sha]';
             const $odooFields = this.$editable.find(odooFieldSelector);
             const renderingClassesSelector = this.odooEditor.options.renderingClasses
                 .map(className => `.${className}`).join(", ");
@@ -1270,8 +1270,8 @@ export class Wysiwyg extends Component {
                             .filter('[data-oe-field="' + $node.data('oe-field') + '"]');
                     }
 
-                    if ($node.data('oe-translation-initial-sha')) {
-                        $nodes = $nodes.filter('[data-oe-translation-initial-sha="' + $node.data('oe-translation-initial-sha') + '"]');
+                    if ($node.data('oe-translation-source-sha')) {
+                        $nodes = $nodes.filter('[data-oe-translation-source-sha="' + $node.data('oe-translation-source-sha') + '"]');
                     }
                     if ($node.data('oe-type')) {
                         $nodes = $nodes.filter('[data-oe-type="' + $node.data('oe-type') + '"]');
@@ -2400,17 +2400,17 @@ export class Wysiwyg extends Component {
      * @override
      */
     _saveTranslationElement($el, context, withLang = true) {
-        if ($el.data('oe-translation-initial-sha')) {
+        if ($el.data('oe-translation-source-sha')) {
             const $els = $el;
             const translations = {};
             translations[context.lang] = Object.assign({}, ...$els.toArray().map(
                 (x) => ({
-                    [$(x).data('oe-translation-initial-sha')]: this._getEscapedElement($(x)).html()
+                    [$(x).data('oe-translation-source-sha')]: this._getEscapedElement($(x)).html()
                 })
             ));
             return this.orm.call(
                 $els.data('oe-model'),
-                'update_field_translations_sha',
+                'web_update_field_translations',
                 [
                     [+$els.data('oe-id')],
                     $els.data('oe-field'),

addons/mail/models/mail_render_mixin.py
@@ -106,8 +106,8 @@ class MailRenderMixin(models.AbstractModel):
             self._check_access_right_dynamic_template()
         return True
 
-    def _update_field_translations(self, fname, translations, digest=None):
-        res = super()._update_field_translations(fname, translations, digest)
+    def _update_field_translations(self, fname, translations, digest=None, source_lang=None):
+        res = super()._update_field_translations(fname, translations, digest=digest, source_lang=source_lang)
         if self._unrestricted_rendering:
             for lang in translations:
                 # If the rendering is unrestricted (e.g. mail.template),

addons/web/static/src/views/fields/translation_dialog.js
@@ -92,8 +92,7 @@ export class TranslationDialog extends Component {
                     if (!translations[term.lang]) {
                         translations[term.lang] = {};
                     }
-                    const oldTermValue = term.value ? term.value : term.source;
-                    translations[term.lang][oldTermValue] = updatedTermValue || term.source;
+                    translations[term.lang][term.source] = updatedTermValue || term.source;
                 } else {
                     translations[term.lang] = updatedTermValue || false;
                 }

addons/web/static/tests/views/fields/html_field.test.js
@@ -181,7 +181,7 @@ test("field html translatable", async () => {
                     en_US: { "first paragraph": "first paragraph modified" },
                     fr_BE: {
                         "first paragraph": "premier paragraphe modifié",
-                        "deuxième paragraphe": "deuxième paragraphe modifié",
+                        "second paragraph": "deuxième paragraphe modifié",
                     },
                 },
             ],

addons/web_editor/models/models.py
@@ -19,10 +19,12 @@ class Base(models.AbstractModel):
 class BaseModel(models.AbstractModel):
     _inherit = 'base'
 
-    def update_field_translations_sha(self, fname, translations):
+    def web_update_field_translations(self, fname, translations):
         field = self._fields[fname]
+        source_lang = None
         if callable(field.translate):
             for translation in translations.values():
                 for key, value in translation.items():
                     translation[key] = field.translate.term_converter(value)
-        return self._update_field_translations(fname, translations, lambda old_term: sha256(old_term.encode()).hexdigest())
+            source_lang = self._get_base_lang()
+        return self._update_field_translations(fname, translations, lambda old_term: sha256(old_term.encode()).hexdigest(), source_lang=source_lang)

addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -4555,8 +4555,8 @@ class SnippetsMenu extends Component {
         }
         this.state.showToolbar = !(!range ||
             !$currentSelectionTarget.parents('#wrapwrap, .iframe-editor-wrapper').length ||
-            closestElement(selection.anchorNode, '[data-oe-model]:not([data-oe-type="html"]):not([data-oe-field="arch"]):not([data-oe-translation-initial-sha])') ||
-            closestElement(selection.focusNode, '[data-oe-model]:not([data-oe-type="html"]):not([data-oe-field="arch"]):not([data-oe-translation-initial-sha])') ||
+            closestElement(selection.anchorNode, '[data-oe-model]:not([data-oe-type="html"]):not([data-oe-field="arch"]):not([data-oe-translation-source-sha])') ||
+            closestElement(selection.focusNode, '[data-oe-model]:not([data-oe-type="html"]):not([data-oe-field="arch"]):not([data-oe-translation-source-sha])') ||
             (e && $(e.target).closest('.fa, img').length ||
             this.options.wysiwyg.lastMediaClicked && $(this.options.wysiwyg.lastMediaClicked).is('.fa, img')) ||
             (this.options.wysiwyg.lastElement && !this.options.wysiwyg.lastElement.isContentEditable)

addons/website/models/ir_ui_view.py
@@ -511,8 +511,8 @@ class View(models.Model):
             res['website_id'] = website_id
         return res
 
-    def _update_field_translations(self, fname, translations, digest=None):
-        return super(View, self.with_context(no_cow=True))._update_field_translations(fname, translations, digest)
+    def _update_field_translations(self, fname, translations, digest=None, source_lang=None):
+        return super(View, self.with_context(no_cow=True))._update_field_translations(fname, translations, digest=digest, source_lang=source_lang)
 
     def _get_base_lang(self):
         """ Returns the default language of the website as the base language if the record is bound to it """

addons/website/static/src/components/translator/translator.js
@@ -99,13 +99,13 @@ export class TranslatorInfoDialog extends Component {
     }
 }
 
-const savableSelector = '[data-oe-translation-initial-sha], ' +
+const savableSelector = '[data-oe-translation-source-sha], ' +
     '[data-oe-model][data-oe-id][data-oe-field], ' +
-    '[placeholder*="data-oe-translation-initial-sha="], ' +
-    '[title*="data-oe-translation-initial-sha="], ' +
-    '[value*="data-oe-translation-initial-sha="], ' +
-    'textarea:contains(data-oe-translation-initial-sha), ' +
-    '[alt*="data-oe-translation-initial-sha="]';
+    '[placeholder*="data-oe-translation-source-sha="], ' +
+    '[title*="data-oe-translation-source-sha="], ' +
+    '[value*="data-oe-translation-source-sha="], ' +
+    'textarea:contains(data-oe-translation-source-sha), ' +
+    '[alt*="data-oe-translation-source-sha="]';
 
 export class WebsiteTranslator extends WebsiteEditorComponent {
     setup() {
@@ -169,10 +169,10 @@ export class WebsiteTranslator extends WebsiteEditorComponent {
         const self = this;
         var attrs = ['placeholder', 'title', 'alt', 'value'];
         const $editable = this.getEditableArea();
-        const translationRegex = /<span [^>]*data-oe-translation-initial-sha="([^"]+)"[^>]*>(.*)<\/span>/;
+        const translationRegex = /<span [^>]*data-oe-translation-source-sha="([^"]+)"[^>]*>(.*)<\/span>/;
         let $edited = $();
         attrs.forEach((attr) => {
-            const attrEdit = $editable.filter('[' + attr + '*="data-oe-translation-initial-sha="]').filter(':empty, input, select, textarea, img');
+            const attrEdit = $editable.filter('[' + attr + '*="data-oe-translation-source-sha="]').filter(':empty, input, select, textarea, img');
             attrEdit.each(function () {
                 var $node = $(this);
                 var translation = $node.data('translation') || {};
@@ -195,7 +195,7 @@ export class WebsiteTranslator extends WebsiteEditorComponent {
             });
             $edited = $edited.add(attrEdit);
         });
-        const textEdit = $editable.filter('textarea:contains(data-oe-translation-initial-sha)');
+        const textEdit = $editable.filter('textarea:contains(data-oe-translation-source-sha)');
         textEdit.each(function () {
             var $node = $(this);
             var translation = $node.data('translation') || {};
@@ -229,7 +229,7 @@ export class WebsiteTranslator extends WebsiteEditorComponent {
         // Hack: we add a temporary element to handle option's text
         // translations from the linked <select/>. The final values are
         // copied to the original element right before save.
-        $editable.filter('[data-oe-translation-initial-sha] > select').each((index, select) => {
+        $editable.filter('[data-oe-translation-source-sha] > select').each((index, select) => {
             const selectTranslationEl = document.createElement('div');
             selectTranslationEl.className = 'o_translation_select';
             const optionNames = [...select.options].map(option => option.text);
@@ -294,7 +294,7 @@ export class WebsiteTranslator extends WebsiteEditorComponent {
             if (translationEl.closest('.s_table_of_content_navbar_wrap')) {
                 // Make sure the same translation ids are used.
                 const href = translationEl.closest('a').getAttribute('href');
-                const headerEl = translationEl.closest('.s_table_of_content').querySelector(`${href} [data-oe-translation-initial-sha]`);
+                const headerEl = translationEl.closest('.s_table_of_content').querySelector(`${href} [data-oe-translation-source-sha]`);
                 if (headerEl) {
                     if (translationEl.dataset.oeTranslationInitialSha !== headerEl.dataset.oeTranslationInitialSha) {
                         // Use the same identifier for the generated navigation

commit d61e49172495a6a8f13327514566b2f2828f39f9

[FIX] web_editor, website_blog: new update translation controller
In the current implementation, updating translations for a `blog.post`
via the route
`/web/dataset/call_kw/blog.post/web_update_field_translations`
fails to apply changes when the post has already been translated once.
This issue is caused by how the system fetches `website.default_lang_id`
from the request.

__Current behavior before commit:__
When editing a previously translated field for `blog.post` or
`product.template`, the system retrieves the website from the request to
determine the `source_lang`. This is used to search for the original
term in the source language and update its translation. However, due to
the `website=True` parameter not being set for the controller,the system
defaults to English (`source_lang = 'en_US'`). As a result:

1. The system searches for the original word in English, even if it
exists in another language.
2. The translation update fails because the original word cannot be
found in English.

__Description of the fix:__
A new controller was introduced to specifically handle translation
updates, ensuring the correct language is fetched and the translation
logic is applied correctly.

__Steps to reproduce the issue:__
1. Add 2 or more languages to the website.
2. Set the default language to any language other than English (US).
3. Create a blog post and write text in the default language.
4. Switch to another language, translate the text and save.
5. Edit the translated text again.
The last edits are not saved.

- bug introduced in: https://github.com/odoo/odoo/commit/06346b049a3a13a9a8b63a4f60151ec24fabb59e
- opw-4239512

addons/web_editor/controllers/main.py
@@ -976,3 +976,8 @@ class Web_Editor(http.Controller):
                 raise UserError(_("Sorry, we could not generate a response. Please try again later."))
         except AccessError:
             raise AccessError(_("Oops, it looks like our AI is unreachable!"))
+
+    @http.route("/web_editor/field/translation/update", type="json", auth="user", website=True)
+    def update_field_translation(self, model, record_id, field_name, translations):
+        record = request.env[model].browse(record_id)
+        return record.web_update_field_translations(field_name, translations)

addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2469,14 +2469,12 @@ export class Wysiwyg extends Component {
                     [$(x).data('oe-translation-source-sha')]: this._getEscapedElement($(x)).html()
                 })
             ));
-            return this.orm.call(
-                $els.data('oe-model'),
-                'web_update_field_translations',
-                [
-                    [+$els.data('oe-id')],
-                    $els.data('oe-field'),
-                    translations,
-                ], { context });
+            return rpc('/web_editor/field/translation/update', {
+                model: $els.data('oe-model'),
+                record_id: [+$els.data('oe-id')],  
+                field_name: $els.data('oe-field'),
+                translations,                
+            });
         } else {
             var viewID = $el.data('oe-id');
             if (!viewID) {

addons/website_blog/tests/test_website_blog_flow.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import json
 
 from odoo.exceptions import UserError
-from odoo.tests.common import users
+from odoo.tests.common import users, HttpCase, tagged
 from odoo.addons.website.tools import MockRequest
 from odoo.addons.website_blog.tests.common import TestWebsiteBlogCommon
 from odoo.addons.portal.controllers.mail import PortalChatter
@@ -115,21 +116,36 @@ class TestWebsiteBlogFlow(TestWebsiteBlogCommon):
         self.assertEqual(self.test_blog_post.teaser, "Test Content...")
 
 
-class TestWebsiteBlogTranslationFlow(TestWebsiteBlogCommon):
-    def setUp(self):
-        self.parseltongue = self.env['res.lang'].create({
+@tagged('-at_install', 'post_install')
+class TestWebsiteBlogTranslationFlow(HttpCase, TestWebsiteBlogCommon):
+    
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.parseltongue = cls.env['res.lang'].create({
             'name': 'Parseltongue',
             'code': 'pa_GB',
             'iso_code': 'pa_GB',
             'url_code': 'pa_GB',
         })
-        self.env["base.language.install"].create({
+        cls.env["base.language.install"].create({
             'overwrite': True,
-            'lang_ids': [(6, 0, [self.parseltongue.id])],
+            'lang_ids': [(6, 0, [cls.parseltongue.id])],
         }).lang_install()
+        cls.headers = {"Content-Type": "application/json"}
+
+    def _build_payload(self, params=None):
+        """
+        Helper to properly build jsonrpc payload
+        """
+        return {
+            "jsonrpc": "2.0",
+            "method": "call",
+            "id": 0,
+            "params": params or {},
+        }
 
     def test_teaser_manual(self):
-        super().setUp()
         blog_post_parseltongue = self.test_blog_post.with_context(lang=self.parseltongue.code)
 
         # No manual teaser, ensure everything works as expected in multi langs
@@ -171,3 +187,37 @@ class TestWebsiteBlogTranslationFlow(TestWebsiteBlogCommon):
         self.assertEqual(blog_post_parseltongue.teaser, "New Parseltongue Content...", "Should still fallback to content")
         self.assertEqual(self.test_blog_post.teaser_manual, "English Teaser Manual")
         self.assertFalse(blog_post_parseltongue.teaser_manual, "Should still be empty")
+
+    def test_update_field_translation(self):
+        """Checks if translated text could be updated"""
+        self.authenticate('admin', 'admin')
+
+        # Setup
+        br_lang = self.env['res.lang']._activate_lang('pt_BR')
+        en_lang = self.env['res.lang']._activate_lang('en_US')
+
+        blog_post = self.env['blog.post'].with_context(lang=br_lang.code).create({
+            'name':'Test Blog',
+            'content':'Todos os blogs', 
+        })
+        # sha256 encoding of 'Todos os blogs'
+        sha = 'c10cb3d9aeec6fe03ed86f24efb262c65ed9de7e9263db1605e3196c343de7a3'
+        
+        blog_post_english = blog_post.with_context(lang=en_lang.code)
+        blog_post_portugues = blog_post.with_context(lang=br_lang.code)
+
+        # Ensure that initial translations for 'en_US' and 'pt_BR' are different
+        blog_post.update_field_translations('content', {en_lang.code: {'Todos os blogs' : 'All blogs'}})
+        self.assertEqual('Todos os blogs', blog_post_portugues.content)
+        self.assertEqual('All blogs', blog_post_english.content)
+        
+        # Test updating translation
+        payload = self._build_payload({
+        'model': blog_post._name,
+        'record_id': blog_post.id,
+        'field_name': 'content',
+        'translations': {en_lang.code: {sha: 'Updated blogs'}},
+        })
+        self.url_open('/web_editor/field/translation/update', data=json.dumps(payload), headers=self.headers)
+        self.assertEqual('Todos os blogs', blog_post_portugues.content)
+        self.assertEqual('Updated blogs', blog_post_english.content)