0741f5b395b1bc708bb42c65eefd09b25cdc6493
K Theja (kthe) <kthe@odoo.com>
2025-01-20 17:14:08 +0530

    [FIX] sale_project: fix recursion in SOL

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -872,8 +872,6 @@ class ProjectTask(models.Model):
                 or task.project_id.sale_order_id
                 or task.sale_order_id
             )
-            if sale_order and not task.partner_id:
-                task.partner_id = sale_order.partner_id
             consistent_partners = (
                 sale_order.partner_id
                 | sale_order.partner_invoice_id
@@ -901,22 +899,31 @@ class ProjectTask(models.Model):
             if task.sale_order_id and task.partner_id.commercial_partner_id not in consistent_partners:
                 task.sale_order_id = task.sale_line_id = False
 
-    @api.depends('sale_line_id.order_partner_id', 'parent_id.sale_line_id', 'project_id.sale_line_id', 'milestone_id.sale_line_id', 'allow_billable')
+    @api.depends('partner_id.commercial_partner_id', 'sale_line_id.order_partner_id', 'parent_id.sale_line_id', 'project_id.sale_line_id', 'milestone_id.sale_line_id', 'allow_billable')
     def _compute_sale_line(self):
         for task in self:
             if not (task.allow_billable or task.parent_id.allow_billable):
                 task.sale_line_id = False
                 continue
-            if not task.sale_line_id:
-                # if the project_id is set then it means the task is classic task or a subtask with another project than its parent.
-                # To determine the sale_line_id, we first need to look at the parent before the project to manage the case of subtasks.
-                # Two sub-tasks in the same project do not necessarily have the same sale_line_id (need to look at the parent task).
-                sale_line = False
-                if task.parent_id.sale_line_id and task.parent_id.partner_id.commercial_partner_id == task.partner_id.commercial_partner_id:
-                    sale_line = task.parent_id.sale_line_id
-                elif task.project_id.sale_line_id and task.project_id.partner_id.commercial_partner_id == task.partner_id.commercial_partner_id:
-                    sale_line = task.project_id.sale_line_id
-                task.sale_line_id = sale_line or task.milestone_id.sale_line_id
+             # Guard against infinite recursion
+            if self._context.get('skip_recursion_compute'):
+                continue
+
+            sale_line = False
+            context_no_recursion = dict(self._context, skip_recursion_compute=True)
+
+            # Determine the sale_line_id
+            if task.parent_id.sale_line_id and task.parent_id.partner_id.commercial_partner_id == task.partner_id.commercial_partner_id:
+                sale_line = task.with_context(context_no_recursion).parent_id.sale_line_id
+            elif task.project_id.sale_line_id and task.project_id.partner_id.commercial_partner_id == task.partner_id.commercial_partner_id:
+                sale_line = task.with_context(context_no_recursion).project_id.sale_line_id
+
+            # If no valid sale_line is found, fall back to milestone's sale_line_id
+            task.sale_line_id = sale_line or task.milestone_id.sale_line_id
+
+            # Validate the computed sale_line_id against the customer
+            if task.sale_line_id and task.sale_line_id.order_partner_id.commercial_partner_id != task.partner_id.commercial_partner_id:
+                task.sale_line_id = False
 
     @api.depends('sale_order_id')
     def _compute_display_sale_order_button(self):
--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -562,7 +562,7 @@ class ProjectTask(models.Model):
             if task.allow_billable and not task.sale_line_id:
                 task.sale_line_id = task.sudo()._get_last_sol_of_customer()
 
-    @api.depends('sale_line_id.order_partner_id', 'parent_id.sale_line_id', 'project_id.sale_line_id', 'allow_billable')
+    @api.depends('partner_id.commercial_partner_id', 'sale_line_id.order_partner_id', 'parent_id.sale_line_id', 'project_id.sale_line_id', 'allow_billable')
     def _compute_sale_line(self):
         super()._compute_sale_line()
         for task in self:
