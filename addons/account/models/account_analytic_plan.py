4e68c92eb229b73e8cfdf4897ec965ddacf03e14
igbe <igbe@odoo.com>
2025-01-20 12:48:33 +0100

    [IMP] account: account analytic plans
    This commit allows the user to put multiple accounts prefix on a single analytic plan line.
    
    So, if the user want to add the accounts 51000, 61000 and 71000, he can now add this: 51, 61, 71 in the same line.
    
    task-4294662

--- addons/account/models/account_analytic_plan.py
+++ addons/account/models/account_analytic_plan.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
+import re
 
-from odoo import fields, models, api
+from odoo import _, fields, models, api
 
 
 class AccountAnalyticApplicability(models.Model):
@@ -18,7 +18,7 @@ class AccountAnalyticApplicability(models.Model):
         },
     )
     account_prefix = fields.Char(
-        string='Financial Accounts Prefix',
+        string='Financial Accounts Prefixes',
         help="Prefix that defines which accounts from the financial accounting this applicability should apply on.",
     )
     product_categ_id = fields.Many2one(
@@ -29,15 +29,35 @@ class AccountAnalyticApplicability(models.Model):
         compute='_compute_display_account_prefix',
         help='Defines if the field account prefix should be displayed'
     )
+    account_prefix_placeholder = fields.Char(compute='_compute_prefix_placeholder')
+
+    @api.depends('account_prefix', 'business_domain')
+    def _compute_prefix_placeholder(self):
+        for applicability in self:
+            account = self.env['account.account'].search(
+                [('account_type', '=', 'expense' if applicability.business_domain == 'bill' else 'income')],
+                limit=1
+            )
+
+            prefix_base = account.code[:2]
+            try:
+                # Convert prefix_base to an integer for numerical manipulation
+                prefix_num = int(prefix_base)
+                account_prefixes = f"{prefix_num}, {prefix_num + 1}, {prefix_num + 2}"
+                applicability.account_prefix_placeholder = _("e.g. %(prefix)s", prefix=account_prefixes)
+
+            except ValueError:
+                applicability.account_prefix_placeholder = "60, 61, 62"
 
     def _get_score(self, **kwargs):
         score = super(AccountAnalyticApplicability, self)._get_score(**kwargs)
         if score == -1:
             return -1
-        product = self.env['product.product'].browse(kwargs.get('product', None))
-        account = self.env['account.account'].browse(kwargs.get('account', None))
+        product = self.env['product.product'].browse(kwargs.get('product'))
+        account = self.env['account.account'].browse(kwargs.get('account'))
         if self.account_prefix:
-            if account and account.code.startswith(self.account_prefix):
+            account_prefixes = tuple(re.split("[,;]", self.account_prefix.replace(" ", "")))
+            if account and account.code.startswith(account_prefixes):
                 score += 1
             else:
                 return -1
